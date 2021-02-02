eccb955dc5d86f05379b3d30909a092c9e16e315
Sherzod Hakimov <sherzod.hakimov@tib.eu>
2021-02-02 20:10:29 +0100

    sample file added

--- preprocess_documents.py
+++ preprocess_documents.py
@@ -41,38 +41,60 @@ def process(input_dir, output_dir, language):
             continue
 
         file_content = file_utils.read_json_file(input_dir+'/'+file)
-        text= file_content['text']
+
+        processed_outputs = {}
 
         while True:
 
-            spacy_annotations = entity_extraction.get_spacy_annotations(text, language)
-            wikifier_annotations = entity_extraction.get_wikifier_annotations(text, language)
+            for doc_id in file_content:
 
-            if wikifier_annotations['processed'] == True:
+                text = file_content[doc_id]['info']['body'].replace('\n', ' ').strip()
+
+                # maximum number of characters Wikifier can process
+                max_char = 20000
+                number_of_queries = int(len(text) / max_char) +1
+
+                wikifier_annotations = {'annotations':[]}
+                for i in range(0, number_of_queries):
+                    start_index, end_index = i*max_char, -1
+                    if i != number_of_queries -1:
+                        end_index = (i + 1) * max_char
+                    wikifier_text = text[start_index:end_index]
+                    wikifier_output = entity_extraction.get_wikifier_annotations(wikifier_text, language)
+
+                    if wikifier_output['processed'] == True:
+
+                        # extract each annotation and add to the global list
+                        for wiki_anno in wikifier_output['annotations']:
+                            wikifier_annotations['annotations'].append(wiki_anno)
+                    else:
+                        print("Rate limit is reached, sleeping for 10 mins")
+                        time.sleep(10 * 60)
+
+                spacy_annotations = entity_extraction.get_spacy_annotations(text, language)
 
                 linked_entities = entity_extraction.link_annotations(spacy_annotations, wikifier_annotations)
                 linked_entities = entity_extraction.fix_entity_types(linked_entities, event_list)
 
                 # save the content
-                output = {"text": text, "entities": linked_entities}
-                output_json = json.dumps(output)
-                file_utils.save_string_to_file(output_json, output_dir+'/'+file)
+                output = {"text": text, "merged_entities": linked_entities, "spacy_entities": spacy_annotations,
+                          "wikifier_entities": wikifier_annotations}
+                processed_outputs[doc_id] = output
 
-                checkpoint_doc_list.append(file)
+            output_json = json.dumps(processed_outputs)
+            file_utils.save_string_to_file(output_json, output_dir + '/' + file)
+            checkpoint_doc_list.append(file)
 
-                file_utils.save_list_to_file(checkpoint_doc_list, output_dir+'/checkpoint.txt')
+            file_utils.save_list_to_file(checkpoint_doc_list, output_dir + '/checkpoint.txt')
 
-                print('Processed: ', len(checkpoint_doc_list),'/',len(input_files))
+            print('Processed: ', len(checkpoint_doc_list), '/', len(input_files))
 
-                break
-            else:
-                print("Rate limit is reached, sleeping for 10 mins")
-                time.sleep(10 * 60)
+            break
 
 def main(argv):
-   input_dir = ''
-   output_dir = ''
-   language = ''
+   input_dir = 'input_dir'
+   output_dir = 'output_dir'
+   language = 'en'
    try:
        opts, args = getopt.getopt(argv,"hi:o:l:",["i_dir=","o_dir=", "lang="])
    except getopt.GetoptError:
